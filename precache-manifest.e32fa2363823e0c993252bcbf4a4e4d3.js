b80f70e83567077508f1c0edf465d98ea0e04a93
Ivan Dimov <iv_dimov@abv.bg>
2020-09-20 19:06:53 +0300

    Updates

---
+++ precache-manifest.e32fa2363823e0c993252bcbf4a4e4d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1878af7722912be6b21849f9daebb48",
+    "url": "/spotify-music-search-app/index.html"
+  },
+  {
+    "revision": "967a3250a186898a1d3a",
+    "url": "/spotify-music-search-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e6066ea526237e63cbb0",
+    "url": "/spotify-music-search-app/static/css/main.69788557.chunk.css"
+  },
+  {
+    "revision": "967a3250a186898a1d3a",
+    "url": "/spotify-music-search-app/static/js/2.57e499ea.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/spotify-music-search-app/static/js/2.57e499ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6066ea526237e63cbb0",
+    "url": "/spotify-music-search-app/static/js/main.70f1f2fd.chunk.js"
+  },
+  {
+    "revision": "6456cdf784253dc41184",
+    "url": "/spotify-music-search-app/static/js/runtime-main.34534f0b.js"
+  },
+  {
+    "revision": "1b2651924062685b5a98dbdb6331a52b",
+    "url": "/spotify-music-search-app/static/media/music.1b265192.jpeg"
+  }
+]);
